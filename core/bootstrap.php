d28c5f9a34085a5b11bab746fe3b8c73ca542fdd
thienchuong84 <thienchuong84@gmail.com>
2017-08-11 17:00:48 +0700

    Chapter 21 Composer Autoloading
    Bài này rất hay, nó chỉ phần basic của composer dùng để autoload các class.
    First install composer , then create composer.json file and add
    {
    	"autoload": {
    		"classmap": [
    			"./"
    			// "controllers/"
    		]
    	}
    }
    Then open cmd and type : composer update
    Nó sẽ generate tạo folder vendor và các thư mục con. Lúc này ta có thể vào file core/bootstrap.php comment các dòng require class lại. Vào index.php và thêm đúng 1 dòng sau để nó tự auto load class

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,12 +5,12 @@ $app['config'] = require 'config.php';
 
 require 'functions.php';
 
-require 'Task.php';
+// require 'Task.php';
 
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
+// require 'core/Router.php';
+// require 'core/Request.php';
+// require 'core/database/Connection.php';
+// require 'core/database/QueryBuilder.php';
 
 
 // return  new QueryBuilder( Connection::make($app['config']['database']) );
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require 'vendor/autoload.php';
 
 // $database = require 'core/bootstrap.php';
 require 'core/bootstrap.php';
